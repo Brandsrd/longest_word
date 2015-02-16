0aaa56d1a023d3cacdf2bcc9513dc8ed5621a8c5
Ruben Brands <rdbrands@gmail.com>
2015-02-16 19:20:48 +0100

    last changes

--- longest_word/config/routes.rb
+++ longest_word/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'longestword#game'
+
+  get 'game' => 'longestword#game'
+
+  get 'score' => 'longestword#score'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
